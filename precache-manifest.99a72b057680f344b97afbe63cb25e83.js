0dd7c0596d0c659ed9c5265f46dd6d2896a93e2f
Vincent Gao <a202898@fmr.com>
2019-12-21 13:36:57 -0500

    Updates

---
+++ precache-manifest.99a72b057680f344b97afbe63cb25e83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470eb940504e17504d3ec806b242b901",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "db3aab3d7244a5b2fe4b",
+    "url": "/monsters-rolodex/static/css/main.f5976dbb.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db3aab3d7244a5b2fe4b",
+    "url": "/monsters-rolodex/static/js/main.59f60474.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
